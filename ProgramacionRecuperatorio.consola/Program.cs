1a068aa626f2ba4d83b71b6573e29fb967cd485c
Federico Rosenberger <federosen2004@outlook.com>
2023-08-22 19:31:54 -0300

    Agregar archivos de proyecto.

---
+++ ProgramacionRecuperatorio.consola/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace ProgramacionRecuperatorio.consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidadPrismas = 0;
+            double basePrisma, alturaPrisma, DistanciaPrisma;
+
+            do
+            {
+
+                Console.Write("Ingrese la base del prisma: ");
+                while (!double.TryParse(Console.ReadLine(), out basePrisma) || basePrisma < 0)
+                {
+                    Console.Write("Valor no válido. Ingrese un valor positivo para la base: ");
+                }
+
+                if (basePrisma != 0)
+                {
+                    Console.Write("Ingrese la altura del prisma: ");
+                    while (!double.TryParse(Console.ReadLine(), out alturaPrisma) || alturaPrisma < 0)
+                    {
+                        Console.Write("Valor no válido. Ingrese un valor positivo para la altura: ");
+                    }
+
+                    Console.Write("Ingrese la distancia del prisma: ");
+                    while (!double.TryParse(Console.ReadLine(), out DistanciaPrisma) || DistanciaPrisma < 0)
+                    {
+                        Console.Write("Valor no válido. Ingrese un valor positivo para la longitud: ");
+                    }
+
+                    double areaBase = CalcularAreaBase(basePrisma, alturaPrisma);
+                    double areaLateral = CalcularAreaLateral(DistanciaPrisma, alturaPrisma);
+                    double volumen = CalcularVolumen(areaBase, alturaPrisma);
+                    double diagonal = CalcularDiagonal(basePrisma, alturaPrisma);
+
+                    Console.WriteLine($"Área Base: {areaBase}");
+                    Console.WriteLine($"Área Lateral: {areaLateral}");
+                    Console.WriteLine($"Volumen: {volumen}");
+                    Console.WriteLine($"Diagonal: {diagonal}");
+
+                    cantidadPrismas++;
+                }
+            } while (basePrisma != 0);
+
+            Console.WriteLine($"Cantidad de prismas ingresados: {cantidadPrismas}");
+
+        }
+        static double CalcularAreaBase(double basePrisma, double alturaPrisma)
+        {
+            return 2 * (basePrisma * alturaPrisma);
+        }
+
+        static double CalcularAreaLateral(double DistanciaPrisma, double alturaPrisma)
+        {
+            return 3 * (DistanciaPrisma * alturaPrisma);
+        }
+
+        static double CalcularVolumen(double areaBase, double alturaPrisma)
+        {
+            return areaBase * alturaPrisma;
+        }
+
+        static double CalcularDiagonal(double basePrisma, double alturaPrisma)
+        {
+            return Math.Sqrt(Math.Pow(basePrisma, 2) + Math.Pow(alturaPrisma, 2));
+        }
+
+    }
+}
